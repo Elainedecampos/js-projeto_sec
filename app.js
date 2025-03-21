de4be46b340a46931e78651df0ece76add2c18ab
Elaine R Campos <elainecamposcomercial@outlook.com>
2025-03-20 21:50:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
-//Comentar várias linhas vscodde Javascript Ctrl+K - Ctrl +C
-//let titulo = document.querySelector('h1');
-//titulo.innerHTML='Gatobô da adivinhação';
-//let subtitulo = document.querySelector('p');
-//subtitulo.innerHTML='Escolha um número entre 1 e 100';
-
-exibirTextoNaTela ('h1','Gatobô da adivinhação');
-exibirTextoNaTela ('p','Escolha um número entre 1 e 100');
-function exibirTextoNaTela (tag,texto){
-    let campo = document.querySelector(tag);
-    campo.innerHTML=texto;
-}
-function verificarChute() {
-    console.log('Apertou o botão');
-}
-=======
 // let titulo = document.querySelector('h1');
 // titulo.innerHTML = 'Jogo de adivinhação';
 
 // let subtitulo = document.querySelector('p');
 // subtitulo.innerHTML = 'Escolha um número entre 1 e 100';
 
+//Ctrl+K - Ctrl+C Comentar várias linhas
+
 exibirTextoNaTela('h1', 'Jogo de adivinhação');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
+function gerarNumeroAleatorio (){
+    return parseInt(Math.random()*100)+1;
+}
+let numeroSecreto= gerarNumeroAleatorio();
+console.log(numeroSecreto);
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
@@ -30,9 +20,9 @@ function exibirTextoNaTela(tag, texto) {
 }
 
 function verificarChute() {
-    console.log('Apertou o botão');
+    let chute = parseInt (document.querySelector('input').value);
+    console.log(chute===numeroSecreto);
 }
 
 //<h1>Jogo de adivinhação</h1>
 //<p>Escolha um número entre 1 e 100</p>
->>>>>>> ed78fcbe0f71bbd87459d9d06682a62e04fd8bfb
